eb2b94c0944e497cb4148ab0967847af1f7dd454
fxcesprit <aleksandr.khom.n@gmail.com>
2025-09-03 09:35:59 +0300

    added basic design

--- geekboards/main/urls.py
+++ geekboards/main/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('.well-known/appspecific/com.chrome.devtools.json', views.chrome_devtools),
+    #GET
+    path('', views.GetDevicesList, name='devices_list_url'),
+    path('device/<int:device_id>', views.GetDeviceInfo, name='device_info_url'),
+    path('user_devices', views.GetDevicesBox, name='devices_box_url'),
+
+    #POST
+    # path('search_device', views.searchDevice, name='search_device_url'),
+
+    #Other
+    path('.well-known/appspecific/com.chrome.devtools.json', views.chrome_devtools), # убирает ошибку GET /.well-known/appspecific/com.chrome.devtools.json HTTP/1.1 404 2710 при открытии кода страницы
 ]
--- geekboards/main/views.py
+++ geekboards/main/views.py
@@ -1,9 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+collection = {
+    'devices' : [
+        {'id': 1, 'name': 'kb1', 'price': 1500, 'short_desc': "kb", 'desc': 'very beautiful kb', 'img': None},
+        {'id': 2, 'name': 'kb2', 'price': 15000, 'short_desc': "kb", 'desc': 'very beautiful kb', 'img': None},
+        {'id': 3, 'name': 'kb3', 'price': 4600, 'short_desc': "kb", 'desc': 'very beautiful kb', 'img': None},
+    ]
+}
+
 # Create your views here.
 def chrome_devtools(request):
+    """убирает ошибку GET /.well-known/appspecific/com.chrome.devtools.json HTTP/1.1 404 2710 при открытии кода страницы"""
     return HttpResponse(status=204)
 
-def index(request):
-    return HttpResponse("<h4>app works</h4>")
+
+def GetDevicesList(request, data=collection):
+    """Рендер страницы девайсов + поиск"""
+    
+    search_text = ''
+    
+    if request.method == 'POST':
+        search_text = request.POST.get('search_text', '').strip()
+    
+    if search_text:
+        filtered_devices = [
+            device for device in collection['devices'] 
+            if search_text.lower() in device['name'].lower()
+        ]
+    else:
+        filtered_devices = collection['devices']
+    
+    data = {
+        'devices': filtered_devices,
+        'search_text': search_text 
+    }
+
+    return render(request, 'main/devices_list.html', data)
+
+
+def GetDeviceInfo(request, device_id, data=collection):
+    """Рендер страницы информации о конкретном девайсе"""
+    return render(request, 'main/device_info.html', {'device': data['devices'][device_id - 1]})
+
+
+def GetDevicesBox(request, user_id):
+    """Рендер страницы корзины девайсов пользователя"""
+    return render(request, 'main/devices_box.html')
+
+
+# def searchDevice(request):
+#     input_text = request.POST['search_text']
+#     data = {'devices': [
+#         device for device in collection['devices'] if input_text in device['name']
+#     ]}
+#     return GetDevicesList(request, data)
